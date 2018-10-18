c9588f39bf01234d1fcd2fc8a455e21837768145
Jason Pulis <jbpulis@yahoo.com>
2018-10-18 18:50:26 -0500

    streamlining
    build better views for the admin

--- mytestsite/locallibrary/catalog/admin.py
+++ mytestsite/locallibrary/catalog/admin.py
@@ -10,18 +10,37 @@ admin.site.register(Genre)
 #admin.site.register(BookInstance)
 
 #Define the admin class
+class BookInline(admin.TabularInline):
+    model = Book
+
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BookInline]
 
 #Register the admin class with the associated model
 admin.site.register(Author, AuthorAdmin)
 
 #Register the Admin classes for Book using the decorator [same as admin.site.register() above]
+class BookInstanceInline(admin.TabularInline):
+    model = BookInstance
+
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre')
+    inlines = [BookInstanceInline]
 
 #Register the Admin classes for BookInstance using the decorator
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('book', 'status', 'due_back')
+    list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
